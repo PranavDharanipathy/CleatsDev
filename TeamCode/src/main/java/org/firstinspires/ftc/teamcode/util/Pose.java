6a77d293e06319f7fbed45a6075ee3669d742386
Pranav Dharanipathy <pranavsdharanipathy@gmail.com>
2026-09-11 08:31:33 -0500

    removed useless code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Pose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Pose.java
@@ -71,7 +71,4 @@ public class Pose {
         return new Pose(pose2D.getX(INCH), pose2D.getY(INCH), pose2D.getHeading(RADIANS));
     }
 
-    public Pose copy() {
-        return new Pose(x, y, heading);
-    }
 }
